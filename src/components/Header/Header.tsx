70efada8f31416ae3078b2c66132c808faebd1c2
Rami-0 <ramysh11@icloud.com>
2023-06-15 01:56:05 +0600

    feat: Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import scss from './Header.module.scss';
+import { useTranslation } from 'react-i18next';
+import { Link, useLocation } from 'react-router-dom';
+import Logo from './assets/Logo.svg';
+
+const Header: React.FC = () => {
+  const { t } = useTranslation();
+  const translationPath = 'Header.';
+  const { pathname } = useLocation();
+  const nav_links = [
+    {
+      path: 'Rent',
+      title: t(`${translationPath}Rent`)
+    },
+    {
+      path: 'Purchase',
+      title: t(`${translationPath}Purchase`)
+    },
+    {
+      path: 'Sale',
+      title: t(`${translationPath}Sale`)
+    },
+    {
+      path: 'Realtors',
+      title: t(`${translationPath}Realtors`)
+    },
+    {
+      path: 'Developers',
+      title: t(`${translationPath}Developers`)
+    }
+  ];
+  const [currnetPage, setCurrentPage] = useState<string>();
+
+  useEffect(() => {
+    if (pathname === '/') {
+      setCurrentPage('Rent');
+    } else {
+      setCurrentPage(pathname.split('/')[1]);
+    }
+  }, [pathname]);
+  return (
+    <header className={scss['header']}>
+      <span className={scss['header__logo']}>
+        <img src={Logo} alt="Logo" />
+      </span>
+      <hr />
+      <nav className={scss['header__nav']}>
+        {nav_links.map(({ path, title }, index) => (
+          <Link
+            className={
+              path === currnetPage ? scss['header__nav__item--active'] : scss['header__nav__item']
+            }
+            to={path}
+            key={index}>
+            <p className="Headline">{title}</p>
+          </Link>
+        ))}
+      </nav>
+      <button style={{ marginLeft: 'auto' }} className={scss['header__loginButton']}>
+        login
+      </button>
+      {/* TODO: create button Global */}
+    </header>
+  );
+};
+
+export default Header;
