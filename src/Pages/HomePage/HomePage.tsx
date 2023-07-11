4379802764d97ca8a51d9dbc485633e9dbce6761
Azim-941223 <urmanbetovazim@gmail.com>
2023-07-11 18:36:08 +0600

    feat/building

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Main from '../../components/Main/Main';
 import DiscountSlider from '../../components/DiscountSlider/DiscountSlider';
+import Building from '../../components/Building/Building';
 
 const HomePage: React.FC = () => {
   return (
     <section>
       <Main />
       <DiscountSlider />
+      <Building />
     </section>
   );
 };
---
+++ src/components/BuildingCard/BuildingCard.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { BuildingsCard } from '../types/types';
+import scss from './BuildingCard.module.scss';
+import { useTranslation } from 'react-i18next';
+
+const BuildingCard: React.FC<BuildingsCard> = ({
+  logo,
+  house,
+  companyName,
+  description,
+  pathCompany
+}) => {
+  const { t } = useTranslation(['Building']);
+  return (
+    <div className={scss['card_container']}>
+      <div className={scss['pic_card']}>
+        <img className={scss['card_logo']} src={logo} alt="logotype" />
+        <img className={scss['card_img']} src={house} alt="House img" />
+      </div>
+      <div className={scss['about_card']}>
+        <h2>{companyName}</h2>
+        <p>{description}</p>
+        <div className={scss['about_link']}>
+          <Link to={pathCompany}>{t('Building.sait')}</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BuildingCard;
